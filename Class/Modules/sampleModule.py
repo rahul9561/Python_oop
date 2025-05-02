f42d8a8ca7240ed381061bba4c5928bd87a4d22d
Suraj Kumar Sah <surajkumarsah2060@gmail.com>
2025-05-02 10:29:43 +0530

    Modules and Packages

---
+++ Class/Modules/sampleModule.py
@@ -0,0 +1,45 @@
+# Modules
+# Definition: A module is a file containing Python definitions and statements. The file name is the module name with the suffix .py appended.
+
+# Steps to create a Module:
+# 1) Create a new python file.
+# 2) Write your code in that file.
+# 3) Save it as sampleModule.py
+# 4) Import this file into another python program using import statement.
+# 5) Use functions defined inside the imported module by calling them using dot operator.
+
+
+# Steps to use a Module:
+# 1) Open a new python file.
+# 2) Import the required module using import statement.
+# 3) Call the function defined inside the imported module using dot operator.
+# 4) Run the program.
+
+
+
+# Steps to Create a Package
+# 1) Create a folder.
+# 2) Inside the folder create __init__.py file.
+# 3) Add all the modules you want to include in package inside the folder.
+# 4) Now you can import any of these modules from outside the package using import statement.
+# 5) Run the program.
+
+
+# Steps to use a Package
+# 1) Open a new python file.
+# 2) Import the required module using import statement.
+# 3) Call the function defined inside the imported module using dot operator.
+# 4) Run the program.
+
+
+def sum(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
---
+++ Class/Modules/sampleUseModule.py
@@ -0,0 +1,10 @@
+import sampleModule as sm
+
+print("Hello World")
+
+print("Sum : ", sm.sum(10,2))
+print("Sub : ", sm.sub(10,2))
+print("Mul : ", sm.mul(10,2))
+print("Div : ", sm.div(10,2))
+
+print(sm.__name__) # sampleModule
